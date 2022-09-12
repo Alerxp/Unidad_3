9930622ddfb6b787e578d8f84e49e70dcd595071
Alexander Nuñez <82907784+Alerxp@users.noreply.github.com>
2022-09-11 23:55:48 -0300

    Add files via upload

---
+++ Ejercicio 1/main.py
@@ -0,0 +1,10 @@
+from ListaSecuencialPorContenido import Lista
+
+if __name__ == '__main__':
+    l = Lista(3)
+    l.insertar(5)
+    l.insertar(3)
+    l.insertar(1)
+    l.insertar(4)
+    l.insertar(2)
+    l.recorrer()
